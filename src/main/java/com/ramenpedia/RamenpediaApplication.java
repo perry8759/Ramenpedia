cf37906068d9c84a595ccd0f05b58e1140dc254e
Perry Guo <perry8759@gmail.com>
2024-04-28 18:56:44 +0800

    init

---
+++ src/main/java/com/ramenpedia/RamenpediaApplication.java
@@ -0,0 +1,13 @@
+package com.ramenpedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamenpediaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RamenpediaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ramenpedia/RamenpediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramenpedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RamenpediaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
