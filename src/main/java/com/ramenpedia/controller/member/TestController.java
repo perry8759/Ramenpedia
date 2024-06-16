12232c41f3406687f05721674e6f0588a7e1a755
perry0705 <perry@bitstreetx.com>
2024-06-16 14:58:52 +0800

    feat: mysql ssl connect

--- src/main/java/com/ramenpedia/controller/member/TestController.java
+++ src/main/java/com/ramenpedia/controller/member/TestController.java
@@ -1,14 +1,22 @@
 package com.ramenpedia.controller.member;
 
 import com.ramenpedia.base.ApiResponse;
+import com.ramenpedia.entity.Member;
+import com.ramenpedia.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("test")
 public class TestController {
 
+    @Autowired
+    private MemberRepository memberRepository;
+
     @GetMapping("/hello")
     public ApiResponse<String> hello() throws Exception {
+        memberRepository.save(Member.create("test", "test", "test", ""));
+
         return ApiResponse.getSuccessInstance("hello");
     }
 }
