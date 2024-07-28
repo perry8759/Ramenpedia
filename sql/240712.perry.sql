94631153dff0e3e577718ae22c6b1a630726ae22
perry0705 <perry@bitstreetx.com>
2024-07-28 17:24:11 +0800

    feat: alter member table column type and add new column nick name

--- sql/240712.perry.sql
+++ sql/240712.perry.sql
@@ -6,3 +6,13 @@ ALTER TABLE `member`
 
 ALTER TABLE `member`
     ADD COLUMN `create_millis` BIGINT(20);
+
+ALTER TABLE `member`
+    ADD COLUMN `uid` VARCHAR(10),
+    ADD CONSTRAINT `unq_member_uid` UNIQUE (`uid`);
+
+ALTER TABLE `member`
+    ADD COLUMN `nickname` VARCHAR(10);
+
+ALTER TABLE `member`
+    MODIFY `birthday` varchar(20) NULL;
